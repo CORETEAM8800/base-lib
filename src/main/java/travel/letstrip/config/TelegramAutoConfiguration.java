aa40f148d973cefe97b6c2bbed115a632fda537f
OtabekHoshimxon <otabekhoshimxon@gmail.com>
2025-12-16 11:45:23 +0500

    add telegram bot

---
+++ src/main/java/travel/letstrip/config/TelegramAutoConfiguration.java
@@ -0,0 +1,10 @@
+package travel.letstrip.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import travel.letstrip.telegram.TelegramProperties;
+
+@Configuration
+@EnableConfigurationProperties(TelegramProperties.class)
+public class TelegramAutoConfiguration {
+}
